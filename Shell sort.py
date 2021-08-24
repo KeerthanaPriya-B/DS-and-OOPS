3595770232f1770a9dd1437af3397f787b4f2395
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2021-08-24 03:11:57 -0700

    Update Shell sort.py

--- Shell sort.py
+++ Shell sort.py
@@ -1,42 +1,24 @@
 # Python program for implementation of Shell Sort
-def shellSort(arr):
-	# Start with a big gap, then reduce the gap
-	n = len(arr)
-	gap = n/2
-	# Do a gapped insertion sort for this gap size.
-	# The first gap elements a[0..gap-1] are already in gapped
-	# order keep adding one more element until the entire array
-	# is gap sorted
-	while gap > 0:
-		for i in range(gap,n):
-			# add a[i] to the elements that have been gap sorted
-			# save a[i] in temp and make a hole at position i
-			temp = arr[i]
+def shellsort(list):
+    gap = len(list) // 2
+    while gap>0:
+        for index in range(gap ,len(list)):
+            current_ele = list[index]
+            pos = index
+            while pos >= gap and current_ele < list[pos-gap]:
+                list[pos] = list[pos-gap]
+                pos = pos-gap
+            list[pos] = current_ele
+        gap = gap//2
 
-			# shift earlier gap-sorted elements up until the correct
-			# location for a[i] is found
-			j = i
-			while j >= gap and arr[j-gap] >temp:
-				arr[j] = arr[j-gap]
-				j -= gap
+num = int(input('List Length:'))
+list = [int(input()) for i in range(num)]
+shellsort(list)
+print('sorted list: ',list)
 
-			# put temp (the original a[i]) in its correct location
-			arr[j] = temp
-		gap /= 2
-
-# Driver code to test above
-arr = [ 12, 34, 54, 2, 3]
-n = len(arr)
-print ("Array before sorting:")
-for i in range(n):
-	print(arr[i])
-shellSort(arr)
-print ("\nArray after sorting:")
-for i in range(n):
-	print(arr[i])
-
-'''Output:
-Array before sorting:
-12 34 54 2 3 
-Array after sorting:
-2 3 12 34 54'''
+'''output:
+list length:3
+5
+9
+1
+sorted list: [1, 5, 9]'''
