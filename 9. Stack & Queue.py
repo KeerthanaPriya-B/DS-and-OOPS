4b4db09551db438d64fe8d7efbdee78c27f5c095
KeerthanaPriya-B <priyakeerthana21@gmail.com>
2021-08-27 06:23:59 -0700

    Update and rename stack.py to 9. Stack & Queue.py

---
+++ 9. Stack & Queue.py
@@ -0,0 +1,28 @@
+# STACK
+
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack)
+
+
+
+# QUEUE
+
+queue=[]
+queue.append('a')
+queue.append('b')
+queue.append('c')
+print(queue)
+
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue)
+
